97cd4383b854ad401d6e9d20f1006f372da52d03
wicaksonoindra <wicaksonoindra66@gmail.com>
2022-09-04 11:54:03 +0700

    last episode 2

---
+++ EP2-bestdayever-ver4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=$1
+
+echo "Good morning $name!!"
+sleep 1
+echo "You're looking good today $name!!"
+sleep 1
+echo "You have the best hair I've ever seen $name!!"
---
+++ EP2-bestdayever-ver5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name=$1
+compliment=$2
+
+echo "Good morning $name!!"
+sleep 1
+echo "You're looking good today $name!!"
+sleep 1
+echo "You have the best $compliment I've ever seen $name!!"
